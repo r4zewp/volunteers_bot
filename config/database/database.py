17056f5ce7808e5a967ea8fdd5d03525f8bb489f
KirillZV9 <kirillzrev79@gmail.com>
2024-05-03 01:25:20 +0300

    :(

--- config/database/database.py
+++ config/database/database.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 import asyncpg
+from peewee import PostgresqlDatabase
+
 
 load_dotenv()
 
 pg_user = os.getenv('PG_USER')
 pg_pw = os.getenv('PG_PASSWORD')
 pg_db = os.getenv('PG_DATABASE')
+
+db = PostgresqlDatabase('postgres@2.tcp.eu.ngrok.io', user=pg_user, password=pg_pw, host='2.tcp.eu.ngrok.io', port='17785')
 
 async def create_pool():
     return await asyncpg.create_pool(
