3e72604770d414ecf094e5e94a7441dade7ed74d
KirillZV9 <kirillzrev79@gmail.com>
2024-05-03 00:22:47 +0300

    create user/vol funcs

--- config/database/user_queries.py
+++ config/database/user_queries.py
@@ -12,4 +12,38 @@ async def get_user(conn, id):
         }
     else:
         return None
+
+async def create_user(conn, phone_num, username, tg_id):
+
+    query = """
+    INSERT INTO users (phone_number, username, telegram_user_id)
+    VALUES ($1, $2, $3);
+    RETURNING id;
+    """
     
+    user_id = await conn.fetchval(query, phone_num, username, tg_id)
+    
+    await conn.close()
+
+    return user_id
+
+
+async def create_volunteer(conn, uid, vol_info:dict):
+    
+    user = get_user(conn, id)
+
+    if (user):
+
+        query = """
+        INSERT INTO volunteers (user_id, name, surename, middlename, education_type, education_program, course_number)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING v_id
+        """
+        volunteer_id = await conn.fetchval(query, uid, vol_info['name'], vol_info['surename'],
+                            vol_info['middlename'], vol_info['education_type'], vol_info['education_program'],
+                            vol_info['course_number'])
+        await conn.close()
+
+        return volunteer_id
+    
+    else: return None
