91e5d24ae9d23e21bfcea8d702c4ccbb93095cca
r4zewp <marijuanatoys@gmail.com>
2024-06-09 22:14:04 +0300

    fixed db connection

--- config/database/database.py
+++ config/database/database.py
@@ -9,23 +9,15 @@ pg_user = os.getenv('PG_USER')
 pg_pw = os.getenv('PG_PASSWORD')
 pg_db = os.getenv('PG_DATABASE')
 
-db = PostgresqlDatabase('postgres@4.tcp.eu.ngrok.io', user=pg_user, password=pg_pw, host='4.tcp.eu.ngrok.io', port='18864')
-
-async def create_pool():
-    return await PostgresqlDatabase(
-        'postgres@167.172.99.57',
+db = PostgresqlDatabase(
+        'postgres',
         user=pg_user,
         password=pg_pw,
         host='167.172.99.57',
-        port='22'
+        port=5432
     )
 
+async def create_pool():
+    return db
 
-# async def create_pool():
-#     return await asyncpg.create_pool(
-#         user=pg_user,
-#         password=pg_pw,
-#         database=pg_db,
-#         host='localhost'
-#     )
 
