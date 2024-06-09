dd5bcbe2e3fb12ab2ef9817462daa9fd0b9287f9
KirillZV9 <kirillzrev79@gmail.com>
2024-06-10 00:15:45 +0300

    asa

--- config/database/user_queries.py
+++ config/database/user_queries.py
@@ -1,20 +1,22 @@
-from config.models.User import User
-from config.models.Volunteer import Volunteer
-
+import asyncpg
+from models.User import User
+from models.Volunteer import Volunteer
+from database import objects
 
 # Асинхронная функция для добавления пользователя
-async def add_user(username, phone_number, telegram_id, objects):
+async def add_user(username, phone_number, telegram_id):
     try:
         # Создание нового пользователя и сохранение его в базе данных
-        user = await objects.create(User, username=username, phone_number=phone_number, telegram_id=telegram_id)
+        user = await objects.create(User, username=username, phone_number=phone_number, telegram_user_id=telegram_id)
         return user
     except Exception as e:
         print(f"Error: {e}")
         return None
 
 # Асинхронная функция для получения пользователя по ID
-async def get_user_by_id(user_id, objects):
+async def get_user_by_id(user_id):
     try:
+        # Поиск пользователя по ID
         user = await objects.get(User, User.id == user_id)
         return user
     except User.DoesNotExist:
--- config/models/User.py
+++ config/models/User.py
@@ -5,7 +5,7 @@ class User(Model):
     id = AutoField()
     username = CharField()
     phone_number = CharField()
-    telegram_id = BigIntegerField()
+    telegram_user_id = BigIntegerField()
 
     class Meta:
         database = db  # Использует базу данных, указанную в database.py
