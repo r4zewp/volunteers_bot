53ea2feb7f53cf309eda96a041178c8e86064612
KirillZV9 <kirillzrev79@gmail.com>
2024-06-10 02:10:08 +0300

    as

--- config/database/volunteer_x_event_queries.py
+++ config/database/volunteer_x_event_queries.py
@@ -55,4 +55,49 @@ async def update_hours_credited(vxe_id, new_hours_credited):
         print(f"Error: {e}")
         return None
     
-    
+# Асинхронная функция для подтверждения волонтера на событии
+async def confirm_volunteer_appearance(vxe_id):
+    try:
+        volunteer_x_event = await objects.get(Volunteer_X_Event, Volunteer_X_Event.vxe_id == vxe_id)
+        volunteer_x_event.appeared = True
+        await objects.update(volunteer_x_event)
+        return volunteer_x_event
+    except Volunteer_X_Event.DoesNotExist:
+        print(f"Volunteer_X_Event with ID {vxe_id} does not exist.")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+# Асинхронная функция для получения всех записей Volunteer_X_Event по конкретному волонтеру
+async def get_volunteer_x_events_by_volunteer(volunteer_id):
+    try:
+        volunteer = await objects.get(Volunteer, Volunteer.v_id == volunteer_id)
+        volunteer_x_events = await objects.execute(Volunteer_X_Event.select().where(Volunteer_X_Event.volunteer == volunteer))
+        
+        # Выводим все записи для проверки
+        for vxe in volunteer_x_events:
+            print(f"Volunteer_X_Event ID: {vxe.vxe_id}, Event ID: {vxe.event.e_id}, Hours Credited: {vxe.hours_credited}, Approved: {vxe.approved}, Appeared: {vxe.appeared}")
+
+        return volunteer_x_events
+    except Volunteer.DoesNotExist:
+        print("Volunteer does not exist.")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+# Асинхронная функция для получения всех записей Volunteer_X_Event, у которых appeared = True
+async def get_appeared_volunteer_x_events():
+    try:
+        volunteer_x_events = await objects.execute(Volunteer_X_Event.select().where(Volunteer_X_Event.appeared == True))
+        
+        # Выводим все записи для проверки
+        for vxe in volunteer_x_events:
+            print(f"Volunteer_X_Event ID: {vxe.vxe_id}, Volunteer ID: {vxe.volunteer.v_id}, Event ID: {vxe.event.e_id}, Hours Credited: {vxe.hours_credited}, Approved: {vxe.approved}, Appeared: {vxe.appeared}")
+
+        return volunteer_x_events
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
